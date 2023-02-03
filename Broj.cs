e4b948f8324013c6d4a77123c1df017276e33f77
StefanTesmanovic <tesmanovic166@gmail.com>
2023-02-03 10:45:00 +0100

    Add project files.

---
+++ Broj.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class Broj
+    {
+        public int vrednost;
+        public Broj()
+        {
+
+        }
+        public Broj(string a)
+        {
+            int privremeno = 0;
+            int trenutno = 0;
+            int pom = 0;
+            int val = 0;
+            for (int i=0;i<a.Length;i++)
+            {
+                switch(a[i])
+                {
+                    case 'M':
+                        val = 1000;
+                        if (pom==0)
+                        {
+                            trenutno = trenutno + val;
+                        }
+                        if (val>trenutno)
+                        {
+                            vrednost = vrednost + val - trenutno;
+                            pom = 0;
+                        }
+                        if (val==trenutno)
+                        {
+                            pom++;
+                        }
+                        if (val<trenutno)
+                        {
+                            vrednost = trenutno;
+                        }
+                        break;
+                    case 'D':
+                        val = 500;
+                        break;
+                    case 'C':
+                        val = 100;
+                        break;
+                    case 'L':
+                        val = 50;
+                        break;
+                    case 'X':
+                        val = 10;
+                        break;
+                    case 'V':
+                        val = 5;
+                        break;
+                    case 'I':
+                        val = 1;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        public string broj1, broj2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            broj2 = textBox2.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            broj1 = textBox1.Text;
+        }
+
+    }
+}
